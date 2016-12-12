999b254a2da7270ed14fd44462edea56617b001a
waqar ahmad <waqargpgc@gmail.com>
2016-12-12 10:37:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var myName = "waqar ahmad";
+var letterColors=[red,orange,green,purple];
+drawName(myName, letterColors);
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+bounceBubbles() ;
